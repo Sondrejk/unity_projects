b0855bd7fb63e35466ea39957e948f1dbbaf9aec
sondrejk <sondrejk@gmail.com>
2023-05-28 21:38:43 +0200

    Jumping and pipes

--- Floppy bob/Assets/BirdScript.cs
+++ Floppy bob/Assets/BirdScript.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.name = "Bob bird";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) == true) {
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
     }
 }
