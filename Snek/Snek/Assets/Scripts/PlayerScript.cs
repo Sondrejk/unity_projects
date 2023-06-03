72b396f0442ed51e13196345f51d48b1c5b23269
sondrejk <sondrejk@gmail.com>
2023-06-03 23:10:26 +0200

    Timer based movement

--- Snek/Snek/Assets/Scripts/PlayerScript.cs
+++ Snek/Snek/Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,7 @@ public class PlayerScript : MonoBehaviour
     public float playerWidth;
     public float playerHeight;
     public float timer;
+    public string lastKeyPressed;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,18 +18,35 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Movement
-        if (Input.GetKeyDown(KeyCode.W) == true) {
-            transform.position = new Vector3(transform.position.x, transform.position.y + playerHeight, transform.position.z);
+        // Storing last key pressed
+        if (Input.GetKeyDown(KeyCode.W) == true){
+            lastKeyPressed = "W";
         }
-        if (Input.GetKeyDown(KeyCode.A) == true) {
-            transform.position = new Vector3(transform.position.x - playerWidth, transform.position.y, transform.position.z);
+        if (Input.GetKeyDown(KeyCode.A) == true){
+            lastKeyPressed = "A";
         }
-        if (Input.GetKeyDown(KeyCode.S) == true) {
-            transform.position = new Vector3(transform.position.x, transform.position.y - playerHeight, transform.position.z);
+        if (Input.GetKeyDown(KeyCode.S) == true){
+            lastKeyPressed = "S";
         }
-        if (Input.GetKeyDown(KeyCode.D) == true) {
-            transform.position = new Vector3(transform.position.x + playerWidth, transform.position.y, transform.position.z);
+        if (Input.GetKeyDown(KeyCode.D) == true){
+            lastKeyPressed = "D";
+        }
+
+        // Executing movement based on timer and last key pressed
+        if (timer >= 0.15) {
+            timer = 0;
+            if (lastKeyPressed == "W"){
+                transform.position = new Vector3(transform.position.x, transform.position.y + playerHeight, transform.position.z);
+            }
+            if (lastKeyPressed == "A"){
+                transform.position = new Vector3(transform.position.x - playerWidth, transform.position.y, transform.position.z);
+            }
+            if (lastKeyPressed == "S"){
+                transform.position = new Vector3(transform.position.x, transform.position.y - playerHeight, transform.position.z);
+            }
+            if (lastKeyPressed == "D"){
+                transform.position = new Vector3(transform.position.x + playerWidth, transform.position.y, transform.position.z);
+            }
         }
 
         //Timer
